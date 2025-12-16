e154ef86f96aa840923396f87ad73817fc9677f9
36mins <benc0sl1n@gmail.com>
2025-12-16 17:45:41 +0000

    Log Changes
    Correct the way Logs spawn in and add the damage script (It took me half an hour to figure this out)

--- Assets/Scripts/Enemy/FallingLog.cs
+++ Assets/Scripts/Enemy/FallingLog.cs
@@ -1,23 +1,16 @@
 using UnityEngine;
-
 public class FallingLog : MonoBehaviour
 {
-
-    public Health PlayerHealth;
-
-    void Start()
-    {
-        PlayerHealth = GetComponent<Health>();
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
-
-
         if (collision.gameObject.CompareTag("Player"))
         {
-            PlayerHealth.Damage(40);  //if the player is hit they take damage 
+            Health playerHealth = collision.gameObject.GetComponent<Health>();
+
+            if (playerHealth != null)
+            {
+                playerHealth.Damage(40);    //Player takes damage after being hit
+            }
         }
     }
 }
-
